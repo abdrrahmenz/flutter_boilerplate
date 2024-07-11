58fbb7a7ce2182e3042090292febb2bc1dca318a
Abdurrahman <abdrrahmenz@gmail.com>
2024-07-11 11:00:59 +0700

    update to latest version

--- lib/core/pages/webview.dart
+++ lib/core/pages/webview.dart
@@ -3,6 +3,10 @@ import 'dart:developer';
 import 'package:flutter/material.dart';
 import 'package:flutter_boilerplate/core/core.dart';
 import 'package:webview_flutter/webview_flutter.dart';
+// Import for Android features.
+import 'package:webview_flutter_android/webview_flutter_android.dart';
+// Import for iOS features.
+import 'package:webview_flutter_wkwebview/webview_flutter_wkwebview.dart';
 
 class WebViewPage extends StatefulWidget {
   const WebViewPage({
@@ -28,6 +32,92 @@ class WebViewPage extends StatefulWidget {
 
 class _WebViewPageState extends State<WebViewPage> {
   double progress = 0;
+  late final WebViewController _controller;
+
+  @override
+  void initState() {
+    super.initState();
+    // #docregion platform_features
+    late final PlatformWebViewControllerCreationParams params;
+    if (WebViewPlatform.instance is WebKitWebViewPlatform) {
+      params = WebKitWebViewControllerCreationParams(
+        allowsInlineMediaPlayback: true,
+        mediaTypesRequiringUserAction: const <PlaybackMediaTypes>{},
+      );
+    } else {
+      params = const PlatformWebViewControllerCreationParams();
+    }
+
+    final WebViewController controller =
+        WebViewController.fromPlatformCreationParams(params);
+    // #enddocregion platform_features
+
+    controller
+      ..setJavaScriptMode(JavaScriptMode.unrestricted)
+      ..setBackgroundColor(const Color(0x00000000))
+      ..setNavigationDelegate(
+        NavigationDelegate(
+          onProgress: (int progress) {
+            log('WEBVIEW PROGRESS: $progress', name: 'WEBVIEW');
+            setState(() {
+              this.progress = progress / 100;
+            });
+            debugPrint('WebView is loading (progress : $progress%)');
+          },
+          onPageStarted: (String url) {
+            debugPrint('Page started loading: $url');
+          },
+          onPageFinished: (String url) {
+            debugPrint('Page finished loading: $url');
+          },
+          onWebResourceError: (WebResourceError error) {
+            debugPrint('''
+Page resource error:
+  code: ${error.errorCode}
+  description: ${error.description}
+  errorType: ${error.errorType}
+  isForMainFrame: ${error.isForMainFrame}
+          ''');
+          },
+          onNavigationRequest: (NavigationRequest request) {
+            if (request.url.startsWith('https://www.youtube.com/')) {
+              debugPrint('blocking navigation to ${request.url}');
+              return NavigationDecision.prevent;
+            }
+            debugPrint('allowing navigation to ${request.url}');
+            return NavigationDecision.navigate;
+          },
+          onHttpError: (HttpResponseError error) {
+            debugPrint('Error occurred on page: ${error.response?.statusCode}');
+          },
+          onUrlChange: (UrlChange change) {
+            debugPrint('url change to ${change.url}');
+          },
+          onHttpAuthRequest: (HttpAuthRequest request) {
+            debugPrint('onHttpAuthRequest to ${request.toString()}');
+          },
+        ),
+      )
+      ..addJavaScriptChannel(
+        'Toaster',
+        onMessageReceived: (JavaScriptMessage message) {
+          ScaffoldMessenger.of(context).showSnackBar(
+            SnackBar(content: Text(message.message)),
+          );
+        },
+      )
+      ..loadRequest(Uri.parse(widget.url));
+
+    // #docregion platform_features
+    if (controller.platform is AndroidWebViewController) {
+      AndroidWebViewController.enableDebugging(true);
+      (controller.platform as AndroidWebViewController)
+          .setMediaPlaybackRequiresUserGesture(false);
+    }
+    // #enddocregion platform_features
+
+    _controller = controller;
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -45,24 +135,8 @@ class _WebViewPageState extends State<WebViewPage> {
   }
 
   Widget _buildWebView() {
-    return WebView(
-      initialUrl: widget.url,
-      onProgress: (int progress) {
-        log('WEBVIEW PROGRESS: $progress', name: 'WEBVIEW');
-        setState(() {
-          this.progress = progress / 100;
-        });
-      },
-      navigationDelegate: widget.onUpdateVisitedUri,
-      javascriptMode: JavascriptMode.unrestricted,
-      onWebResourceError: (error) {
-        log(
-          'FAILURE LOAD: ${error.failingUrl} => ${error.description}',
-          name: 'WEBVIEW',
-        );
-      },
-      onWebViewCreated: widget.onWebViewCreated,
-      gestureNavigationEnabled: true,
+    return WebViewWidget(
+      controller: _controller,
     );
   }
 
